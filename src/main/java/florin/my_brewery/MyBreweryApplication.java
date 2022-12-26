0f8ab63a0264161233f7001ffd158916e14e38ae
Florin STRULEA <f.strulea@NeoSoft.local>
2022-12-26 11:39:12 +0100

    initial commit

---
+++ src/main/java/florin/my_brewery/MyBreweryApplication.java
@@ -0,0 +1,13 @@
+package florin.my_brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/florin/my_brewery/MyBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package florin.my_brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

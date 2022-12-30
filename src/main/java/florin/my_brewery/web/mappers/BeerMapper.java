4deb8b53d90e6fe837642caccfaf15584a248876
Florin STRULEA <florin.strulea@neo-soft.fr>
2022-12-30 13:37:46 +0100

    Added date mapper for date type conversion

--- src/main/java/florin/my_brewery/web/mappers/BeerMapper.java
+++ src/main/java/florin/my_brewery/web/mappers/BeerMapper.java
@@ -4,7 +4,7 @@ import florin.my_brewery.domain.Beer;
 import florin.my_brewery.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = {DateMapper.class})
 public interface BeerMapper {
 
   BeerDto beerToBeerDto(Beer beer);
